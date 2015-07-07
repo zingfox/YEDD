bed7f40717923852ace222148509a8f8f2184981
zingfox <zingfox@hotmail.com>
2015-07-07 12:17:44 +0700

    text count

--- app/src/main/java/com/zingfox/yedd/CreateBlogActivity.java
+++ app/src/main/java/com/zingfox/yedd/CreateBlogActivity.java
@@ -2,11 +2,15 @@ package com.zingfox.yedd;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Zing on 7/3/2015.
@@ -14,6 +18,8 @@ import android.widget.EditText;
 public class CreateBlogActivity extends ActionBarActivity {
 
     private EditText title;
+    private EditText story;
+    private TextView textCount;
     private Button post;
     private Context context = this;
 
@@ -23,7 +29,11 @@ public class CreateBlogActivity extends ActionBarActivity {
         setContentView(R.layout.activity_create_blog);
 
         title = (EditText) findViewById(R.id.title);
+        story = (EditText) findViewById(R.id.story);
+        textCount = (TextView) findViewById(R.id.text_count);
         post = (Button) findViewById(R.id.post);
+
+        story.addTextChangedListener(textWatcher);
 
         post.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,4 +44,31 @@ public class CreateBlogActivity extends ActionBarActivity {
             }
         });
     }
+
+    public TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            int count = 140 - story.length();
+
+            textCount.setText(String.valueOf(count));
+
+            textCount.setTextColor(Color.GREEN);
+
+            if(count < 10) {
+                textCount.setTextColor(Color.RED);
+            }else if(count < 20) {
+                textCount.setTextColor(Color.YELLOW);
+            }
+        }
+    };
 }
