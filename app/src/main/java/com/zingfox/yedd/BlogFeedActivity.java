ce698a35a4aafbdc4f096611a858bc035cb3dc6c
zingfox <zingfox@hotmail.com>
2015-07-07 11:14:22 +0700

    something

--- app/src/main/java/com/zingfox/yedd/BlogFeedActivity.java
+++ app/src/main/java/com/zingfox/yedd/BlogFeedActivity.java
@@ -1,10 +1,13 @@
 package com.zingfox.yedd;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class BlogFeedActivity extends ActionBarActivity {
-/*
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,4 @@ public class BlogFeedActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    */
-
 }
--- app/src/main/java/com/zingfox/yedd/CreateBlogActivity.java
+++ app/src/main/java/com/zingfox/yedd/CreateBlogActivity.java
@@ -1,8 +1,10 @@
 package com.zingfox.yedd;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,7 +15,7 @@ public class CreateBlogActivity extends ActionBarActivity {
 
     private EditText title;
     private Button post;
-    private Context context;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +23,15 @@ public class CreateBlogActivity extends ActionBarActivity {
         setContentView(R.layout.activity_create_blog);
 
         title = (EditText) findViewById(R.id.title);
+        post = (Button) findViewById(R.id.post);
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, FunctionActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
