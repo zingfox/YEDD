8c298b5505f41de8238fc8aab0582f2275c1ffd0
zingfox <zingfox@hotmail.com>
2015-07-08 16:50:55 +0700

    feed layout

--- app/src/main/java/com/zingfox/yedd/BlogFeedActivity.java
+++ app/src/main/java/com/zingfox/yedd/BlogFeedActivity.java
@@ -1,16 +1,18 @@
 package com.zingfox.yedd;
 
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 
+import com.zingfox.yedd.model.Blog;
+
+import java.util.List;
 
 public class BlogFeedActivity extends ActionBarActivity {
 
@@ -18,10 +20,10 @@ public class BlogFeedActivity extends ActionBarActivity {
     private FloatingActionButton newBlogBtn;
     private TabLayout tabLayout;
 
-    //private ListView blogListView;
-    //private List<Blog> blogs;
+    private ListView blogListView;
+    private List<Blog> blogs;
     //private FeedAdapter feedAdapter;
-    //private Context context = this;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,53 +42,24 @@ public class BlogFeedActivity extends ActionBarActivity {
         newBlogBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(rootLayout, "YeddddddDDDD", Snackbar.LENGTH_SHORT)
-                        .setAction("Undo", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                //Intent intent = new Intent(context, CreateBlogActivity.class);
-                                //startActivity(intent);
-                                //finish();
-                            }
-                        })
-                .show();
+                Intent intent = new Intent(context, CreateBlogActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
-        //blogListView = (ListView) findViewById(R.id.blog_list);
+        //blogListView = (ListView) findViewById(R.id.listView);
 
         //inputBlog();
         //feedAdapter = new FeedAdapter(this, blogs);
         //blogListView.setAdapter(feedAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_blog_feed, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /*
     private void inputBlog() {
         blogs.add(new Blog("TEST"));
         blogs.add(new Blog("YEDD"));
         blogs.add(new Blog("NOOO"));
     }
-    */
+
 }
 
